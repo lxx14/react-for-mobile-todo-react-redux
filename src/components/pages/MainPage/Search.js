249d9cba9670f850e8cc950634b98dfb3fdab428
lxx14 <lxx1427@gmail.com>
2019-06-03 14:17:26 +0300

    fix placeholder in search

--- src/components/pages/MainPage/Search.js
+++ src/components/pages/MainPage/Search.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Search =() => {
   return (
     <div className='search-container'>
-      <input type='text' />
+      <input type='text' placeholder='search by title'/>
       <button type="button"><i className="fas fa-search"></i></button>
     </div>
   )
