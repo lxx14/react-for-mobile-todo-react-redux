24d17c6e7c8d6e4b14d23906e20f4dcaa823d79d
lxx14 <lxx1427@gmail.com>
2019-06-03 17:20:37 +0300

    starting create functionaliry routing

--- src/components/pages/SinglePage/index.js
+++ src/components/pages/SinglePage/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.scss';
 
 const SinglePage = () => {
   return(
-    <div>
+    <div className='page'>
     <h2>SinglePage</h2>
+    <NavLink to='/'><i className="far fa-arrow-alt-circle-left"></i></NavLink>
     </div>
   )
 }
