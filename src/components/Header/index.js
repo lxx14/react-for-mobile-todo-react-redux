720b0119392d850c069e2e9025bc55635f00c7cb
lxx14 <lxx1427@gmail.com>
2019-06-03 17:31:34 +0300

    add amount of active deals

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './style.scss';
 
-const Header = () => {
+const Header = (props) => {
   return(
     <div className='header'>
     <h2><i className="fas fa-book"></i>notepad</h2>
+    <p>Active deals: <span>{props.deals.length}</span></p>
     </div>
   )
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  deals: state.mainPage.toDos
+})
+
+export default connect(mapStateToProps)(Header);
