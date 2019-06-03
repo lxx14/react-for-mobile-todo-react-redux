1e61b4144bfc92aaeead1ec804adad19faef5dc7
lxx14 <lxx1427@gmail.com>
2019-06-03 14:18:23 +0300

    fix order in todos

--- src/components/pages/MainPage/reducer.js
+++ src/components/pages/MainPage/reducer.js
@@ -16,7 +16,7 @@ export default function (state = initialState, action) {
     case ADD_TODO:
       return {
         ...state,
-        toDos: [...state.toDos, action.data]
+        toDos: [action.data, ...state.toDos]
       }
   }
 
